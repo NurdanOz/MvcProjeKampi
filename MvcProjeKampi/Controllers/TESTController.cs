3c76070507b78dc7b7c6e791d81197616f493429
NurdanOz <nurdanefeozdemir@gmail.com>
2025-03-28 03:53:37 +0300

    Add project files.

---
+++ MvcProjeKampi/Controllers/TESTController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class TESTController : Controller
+    {
+        // GET: TEST
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult test2()
+        {
+            return View();
+        }
+    }
+}
